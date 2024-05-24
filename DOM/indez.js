5547b09a979997cffd551e8166177753d6a0b923
dharani3031 <167017139+dharani3031@users.noreply.github.com>
2024-05-24 10:43:45 +0530

    Add files via upload

---
+++ DOM/indez.js
@@ -0,0 +1,77 @@
+// console.log(window.innerHeight,window.innerWidth);
+// console.log(window.open('https://www.freecodecamp.org/news/what-is-the-dom-document-object-model-meaning-in-javascript/'));
+// console.log(window.close());
+// window.close();
+// 
+// window.alert("something is wrong");
+// console.log(window);
+// window.prompt("what is your name?");
+// console.log(window);
+// window.print()
+// console.log(window);
+
+// console.log(console);
+// console.error("the syntax is wrong!!");
+// console.info("the info is useful");
+// console.warn("type mistake");
+// console.debug("junk");
+
+//screen objects
+
+console.log(screen.height,screen.width);
+console.log()
+
+//location
+
+// console.log(window.location);
+
+// setInterval(func() {
+//     document:title = "hello pageo"
+// },2000);
+
+// setInterval(function()=> 
+//     window:href()
+// )
+
+// console.log(location);
+//  setInterval(()=>{
+//     location.href = "https://hackernoon.com/10-frontend-projects-to-take-your-coding-skills-to-the-next-level";
+//  },5000);
+
+// console.log(history,back
+//     (1)
+// )
+
+// console.log(bcke());
+
+//navigate objects
+
+console.log(navigator);
+
+//DOM documents
+
+// let mydocs = document;
+//  console.log(mydocs);
+//  console.dir(mydocs.body.children[0].children[0]);
+
+console.log(window.find("is"));
+
+console.log(window.find("wow"));
+
+//objects
+
+// const peiyar={
+//     name:"dharani",
+//     place:"tharai",
+//     unpeiyar(){
+//         return 'hello,this is  ${this.name} and my native is ${this.place}';
+    
+//     },
+// };
+
+// const namae= peiyar.unpeiyar();
+// console.log(namae);
+
+document.close();
+document.write("<p>welcome to all</p>");
+document.close();
